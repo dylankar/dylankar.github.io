06f8a6daa1337e321f7d21cab0ff3418d8f111fc
dkario <drk2130@columbia.edu>
2015-06-29 10:51:49 -0400

    Memoriam page w/ ui-router

--- classnotes/js/main.js
+++ classnotes/js/main.js
@@ -1,63 +1,94 @@
-var cnApp = angular.module('cnApp', ['cnFilters']);
-cnApp.factory('Data', function() {
-	console.log(data);
-	return data;
-});
-
+var cnApp = angular.module('cnApp', ['cnFilters', 'ui.router', 'ngRoute']);
 
 cnApp.factory('ClassNotes', function($http) {
-	var ClassNotes = {};
+	return {
+		// Set data from issue
+		issue: 'cnspring2015',
+		notes: null,
 
-	ClassNotes.issue = 'cnspring2015';
-	
-	var dataPath = 'js/' + ClassNotes.issue + '.json';
-	$http.get(dataPath).success(function(data) {
-		ClassNotes.notes = angular.fromJson(data);
-	});
+		getNotes: function(suffix, cb) {
+			var dataPath = 'data/' + this.issue + suffix + '.js';
+			$http.get(dataPath).success(cb);
+		},
 
-	return ClassNotes;
+		fullName: function(note) {
+			return note.FIRST_NAME.trimRight() + ' ' + note.LAST_NAME.trimRight();
+		},
+
+		andersonPhotoURL: function(note, photoNum) {
+			// Filepath
+			// var path = 'img/'+ClassNotes.issue+'/alums_400/';
+			var path = 'img/';
+			// Filename: FirstLast (remove whitespace and periods)
+			var fname = this.clean(note.FIRST_NAME) +
+				this.clean(note.LAST_NAME);
+			// If second photo, add '2' to end: FirstLast2
+			if (note.PHOTO_2 !== null)
+				fname = ((photoNum == 1) ? fname : fname + '2');
+			// Get extension
+			var ext = ((photoNum == 1) ?
+				note.PHOTO_1.substring(note.PHOTO_1.lastIndexOf('.')) :
+				note.PHOTO_2.substring(note.PHOTO_2.lastIndexOf('.')));
+			// For now: if extension is .pdf of .tif, use converted .jpg version
+			ext = ((ext == '.pdf' || ext == '.tif') ? '.jpg' : ext);
+			// Return complete filepath for photo
+			return path + fname + ext;
+		},
+
+		clean: function(name) {
+			return name.replace(/ /g,'')
+				.replace('.','')
+				.replace('(','')
+				.replace(')','');
+		}
+	};
 });
 
-function ClassNotesCtrl($scope, ClassNotes){
-	$scope.classnotes = ClassNotes;
+cnApp.controller('ClassNotesCtrl', function($scope, ClassNotes){
+	$scope.cn = ClassNotes;
 
-	$scope.fullName = function(note) {
-		return note.FIRST_NAME.trimRight() + ' ' + note.LAST_NAME.trimRight();
+	ClassNotes.getNotes('', function(data) {
+		$scope.cn.notes = angular.fromJson(data);
+	});
+});
+
+cnApp.controller('MemoriamCtrl', function($scope, ClassNotes) {
+	$scope.cn = ClassNotes;
+
+	$scope.cn.getNotes('memoriam', function(data) {
+		$scope.cn.notes = angular.fromJson(data);
+	});
+});
+
+cnApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+	$urlRouterProvider.otherwise('/');
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'templates/home.html',
+			controller: 'ClassNotesCtrl'
+		})
+		.state('memoriam', {
+			url: '/memoriam',
+			templateUrl: 'templates/memoriam.html',
+			controller: 'MemoriamCtrl'
+		});
+});
+
+angular.module('cnFilters', [])
+
+.filter('search', function() {
+	return function(input) {
+		return input;
 	};
+})
 
-	$scope.submitDate = function(note) {
-		var d = note.SUBMISSION_DATE.split('/');
-		if (d[1] < 10) d[1] = "0"+d[1];
-		if (d[1] < 10) d[0] = "0"+d[0];
-		console.log("20"+date[2]+date[1]+date[0]);
-		return "20"+date[2]+date[1]+date[0];
+/* Make name of alum in submission bold */
+.filter('boldAlum', function($sce) {
+	return function(str, nameToBold) {
+		nameToBold = nameToBold.replace(/([()[{*+.$^\\|?])/g, '\\$1');
+		str = str.replace(new RegExp(nameToBold, 'gi'),
+		'<span class="note-alumnus">$&</span>');
+		return $sce.trustAsHtml(str);
 	};
-
-	// Gotta adapt for alums_full too
-	// $scope.andersonPhotoURL = function(note, photoNum) {
-	// 	var path = 'img/alums_400/'
-	// 	var str = ((photoNum == 1) ? note.PHOTO_1 : note.PHOTO_2);
-	// 	var n = str.lastIndexOf('/');
-	// 	var fname = str.substring(n+1).replace(/%\d{2}/g, '_');
-	// 	return path + fname;
-	// };
-
-	$scope.andersonPhotoURL = function(note, photoNum) {
-		// Filepath
-		var path = 'img/'+ClassNotes.issue+'/alums_400/';
-		// Filename: FirstLast (remove whitespace and periods)
-		var fname = note.FIRST_NAME.replace(/ /g,'').replace('.','') +
-			note.LAST_NAME.replace(/ /g,'').replace('.','');
-		// If second photo, add '2' to end: FirstLast2
-		if (note.PHOTO_2 !== null)
-			fname = ((photoNum == 1) ? fname : fname + '2');
-		// Get extension
-		var ext = ((photoNum == 1) ?
-			note.PHOTO_1.substring(note.PHOTO_1.lastIndexOf('.')) :
-			note.PHOTO_2.substring(note.PHOTO_2.lastIndexOf('.')));
-		// For now: if extension is .pdf of .tif, use converted .jpg version
-		ext = ((ext == '.pdf' || ext == '.tif') ? '.jpg' : ext);
-		// Return complete filepath for photo
-		return path + fname + ext;
-	};
-}
+});
